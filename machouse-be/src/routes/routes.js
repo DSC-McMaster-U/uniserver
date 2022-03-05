73384e62ecbaa60b8b5d2531cc0fff1e78f8637f
tbrar06 <brart5@mcmaster.ca>
2022-03-04 21:56:42 -0500

    adding issues for sprint1

--- machouse-be/src/routes/routes.js
+++ machouse-be/src/routes/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // require "import" your exported route using the same syntax as the line below
 let user = require('./user');
+let tasks = require('./tasks');
 
 // Add your route here using the same sytax as seen on the line below
 router.use('/user', user);
+router.use('/tasks',tasks);
 
 // DO NOT CHANGE
 export default router;
