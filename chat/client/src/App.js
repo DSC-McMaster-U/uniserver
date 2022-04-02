97bfa42e36e4bf970660de094a20e310be5e5ea4
HarrisonOssias <ossiash@mcmaster.ca>
2022-04-02 04:55:12 -0400

    Challenge Boilerplate

---
+++ chat/client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+	return <>Hello, Please follow the folder structure while designing your application. When you are completed, you can complete the How-to-Run document in markdown.</>;
+}
+
+export default App;
